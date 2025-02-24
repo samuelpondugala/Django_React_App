506ab3a73096912e3d1454185ae1db321d862f61
David Samuel <davidsamuel7143@gmail.com>
2025-02-24 19:28:19 +0530

    Added some pages to view on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home  from './components/Home';
+import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
 
