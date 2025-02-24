e3b8b60c57f2bbec0662efe9a0115fe9590dd5f5
David Samuel <davidsamuel7143@gmail.com>
2025-02-24 19:30:58 +0530

    Implement basic structure for React frontend and set up routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
 import About from './components/About';
+import Home from './components/Home';
+
 import Create from './components/Create';
 
 function App() {
