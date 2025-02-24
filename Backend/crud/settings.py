4ae979073838efad0ed31f2ffef47c540c743279
David Samuel <davidsamuel7143@gmail.com>
2025-02-24 18:05:26 +0530

    Intialized Frontend folder and started react app and located app.js file to customize the components in the app folder and created Components folder for future implications of  components and buttons and installed django cors headers to bypass or to connect front end and back end it executes backend file on frontend

--- Backend/crud/settings.py
+++ Backend/crud/settings.py
@@ -28,10 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,6 +42,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",]
 
 ROOT_URLCONF = 'crud.urls'
 
