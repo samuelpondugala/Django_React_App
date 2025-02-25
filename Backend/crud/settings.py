a3b3d1f29c4efc98fe75ed132811df8bbb64bd9f
David Samuel <davidsamuel7143@gmail.com>
2025-02-25 22:43:14 +0530

    Update STATICFILES_DIRS to STATIC_ROOT in settings.py for static file configuration

--- Backend/crud/settings.py
+++ Backend/crud/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['staticfiles/']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
