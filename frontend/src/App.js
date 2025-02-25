c341925d14d16eab21be0a68f9ba327b6401c61b
David Samuel <davidsamuel7143@gmail.com>
2025-02-25 13:49:08 +0530

    Implement Navbar component and integrate routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import About from './components/About';
+import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
-
+import About from './components/About';
 import Create from './components/Create';
+import Navbar from './components/NavBar';
+//import Edit from './components/Edit';
+//import Delete from './components/Delete';
 
 function App() {
+  const myWidth = 220
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+        <Navbar 
+            drawerWidth={myWidth}
+            content = {
+              <Routes>
+                <Route path="" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/create" element={<Create/>}/>
+                {/* <Route path="/edit" element={<Edit/>}/>
+                <Route path="/delete" element={<Delete/>}/> */}
+              
+              </Routes>
+            }
+        
+        />
+            
+
+
+        
     </div>
   );
 }
